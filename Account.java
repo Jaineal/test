099a5c929706643f7d6753f4c096e0240b67b63f
Jaidan Neal <nealjaidan218@gmail.com>
2025-01-30 09:00:34 -0500

    Removal of Constructor

--- Account.java
+++ Account.java
@@ -15,9 +15,6 @@ public class Account {
 	// New Comment for balance
 	private double balance;
 	private String name;
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
 	
 	public double getBalance() {
 		return balance;
